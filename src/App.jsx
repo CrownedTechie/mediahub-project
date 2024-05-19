d9b357af9f1e8c6217349be680061c6080a80194
CrownedTechie <nenchi00@gmail.com>
2024-05-19 02:35:44 +0100

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import './App.css';
+import Header from './components/Header.jsx'
+
 function App() {
 
   return (
     <>
-      <h1>Media Hub</h1>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import logo from '../assets/MediaHub-logo.png';
+
+
+const Header = () => {
+  return (
+      <>
+        <header>
+            <img src={logo} alt="media hub logo" />
+        </header>
+      </>
+  )
+}
+
+export default Header
