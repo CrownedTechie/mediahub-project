55a0c3904c82816c1a9231e60be3ddde6ee40525
CrownedTechie <nenchi00@gmail.com>
2024-05-19 07:14:53 +0100

    create title component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css';
-import Header from './components/Header.jsx'
+import Header from './components/Header.jsx';
+import Title from './components/Title.jsx';
+
 
 function App() {
 
   return (
     <>
+      {/* I'm creating this for a signup page, I might create another file for it separately */}
       <Header />
+
+      <main>
+        <Title
+          title="Let's get you started"
+          subtitle="Become a MediaHubber today and Keep track of all your Media consumption"
+        />
+      </main>
+
     </>
   )
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,16 @@
+import './Title.css';
+
+
+const Title = ({ title, subtitle }) => {
+    return (  
+        <>
+            <div className='title'>
+                <h1>{ title }</h1>
+                <p>{ subtitle }</p>
+            </div>
+            
+        </>
+    );
+}
+ 
+export default Title;
